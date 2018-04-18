ddaf50fd5d9c8b566f6bb24dffeba373d4322903
Piotr Orlik <piotr.orlik.j@gmail.com>
2018-04-18 14:22:51 +0200

    Base index view, template and styles added.

--- projektPZ/urls.py
+++ projektPZ/urls.py
@@ -18,9 +18,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from projektPZ.views import IndexView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
