82f141f7ec251e060fb0e4ffa16ffe14a52a1073
Piotr <GromQ96@gmail.com>
2018-04-24 15:52:46 +0200

    Validators for User model added as builin ones were not working.

--- custom_auth/validators.py
+++ custom_auth/validators.py
@@ -1,8 +1,28 @@
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from django.conf import settings
 
 
-username_validator = RegexValidator(
+username_regex_validator = RegexValidator(
     regex='^[a-zA-Z0-9]+$',
-    message='Login must contain only lettern and digits.',
+    message='Login must contain only letters and digits.',
     code='invalid_username'
 )
+
+
+def password_length_validator(password):
+    minimum_password_length = settings.MINIMUM_PASSWORD_LENGTH
+    if len(password) < minimum_password_length:
+        raise ValidationError(
+            f'Password is too short- it must be at '
+            f'least {minimum_password_length} characters long.'
+        )
+
+
+def username_length_validator(password):
+    MINIMUM_USERNAME_LENGTH = settings.MINIMUM_USERNAME_LENGTH
+    if len(password) < MINIMUM_USERNAME_LENGTH:
+        raise ValidationError(
+            f'Username is too short- it must be at '
+            f'least {MINIMUM_USERNAME_LENGTH} characters long.'
+        )
