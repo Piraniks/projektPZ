60d81bfb2f650b19e5b3913c79672893c1188c9f
Piotr <GromQ96@gmail.com>
2018-05-30 15:50:37 +0200

    Model modified. Tests adjusted.

--- device/tests/test_models.py
+++ device/tests/test_models.py
@@ -8,7 +8,71 @@ class DeviceTestCase(TransactionTestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='user',
                                              password='password')
-        self.device = Device.objects.create()
+        self.device = Device.objects.create(name='device',
+                                            owner=self.user)
 
     def test_is_up_to_date_with_none_version(self):
-        pass
+        is_up_to_date = self.device.is_up_to_date
+
+        self.assertTrue(is_up_to_date)
+
+    def test_is_up_to_date_with_newest_version(self):
+        new_version = Version.objects.create(versioned_object=self.device,
+                                             name='new_version')
+        self.device.version = new_version
+        self.device.save()
+
+        is_up_to_date = self.device.is_up_to_date
+
+        self.assertTrue(is_up_to_date)
+
+    def test_is_up_to_date_with_old_version(self):
+        new_version = Version.objects.create(versioned_object=self.device,
+                                             name='new_version')
+        old_version = Version.objects.create(versioned_object=self.device,
+                                             name='new_version')
+
+        new_version.previous = old_version
+        old_version.next = new_version
+        new_version.save()
+        old_version.save()
+
+        self.device.version = old_version
+        self.device.save()
+
+        is_up_to_date = self.device.is_up_to_date
+
+        self.assertFalse(is_up_to_date)
+
+    def test_raise_version_with_none_as_latest(self):
+        result = self.device.raise_version()
+
+        self.assertFalse(result)
+
+    def test_raise_version_with_latest_version_already(self):
+        new_version = Version.objects.create(versioned_object=self.device,
+                                             name='new_version')
+        self.device.version = new_version
+        self.device.save()
+
+        result = self.device.raise_version()
+
+        self.assertFalse(result)
+
+    def test_raise_version_with_version_raising(self):
+        new_version = Version.objects.create(versioned_object=self.device,
+                                             name='new_version')
+        old_version = Version.objects.create(versioned_object=self.device,
+                                             name='new_version')
+
+        new_version.previous = old_version
+        old_version.next = new_version
+        new_version.save()
+        old_version.save()
+
+        self.device.version = old_version
+        self.device.save()
+
+        result = self.device.raise_version()
+
+        self.assertTrue(result)
